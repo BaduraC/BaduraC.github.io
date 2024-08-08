7c47259c214ac47f5a9fbc1e5f2c732ddf1a538a
Collin Müller <108384384+BaduraC@users.noreply.github.com>
2024-08-08 13:03:26 +0200

    swiper implementation

---
+++ swiper-init.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    generateQuestion();
+});
